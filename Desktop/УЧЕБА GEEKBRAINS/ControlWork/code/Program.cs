0b8c9d41c660e617260e11c9333f0d3e65e3c0eb
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2022-01-31 21:13:40 +0700

    написал код выполняющий условие задаци

--- Desktop/УЧЕБА GEEKBRAINS/ControlWork/code/Program.cs
+++ Desktop/УЧЕБА GEEKBRAINS/ControlWork/code/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,длинна которых <= 3 символам
+
+
+string[]array = {"dog","water","cat","123" , "586" , "11" ,"world"};
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i]+" ");
+}
+Console.WriteLine();
+   
+string[]newArray = array;
+
+for (int j = 0; j < newArray.Length; j++)
+{
+    if (array[j].Length <= 3) 
+
+        Console.Write(newArray[j]+" ");
+
+}
+
